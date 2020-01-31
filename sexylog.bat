6476c5f9febb0b2e78080466e9f9263365391eca
Davinci-E-182 <davinci.e182@gmail.com>
2020-01-30 21:27:40 -0700

    ::cleaned up syntax

--- sexylog.bat
+++ sexylog.bat
@@ -1,73 +1,61 @@
 @echo off
+
+GOTO menu
+
 :menu
-echo 1. (VirtualBox Host-Only Network)
-echo 2. (Local Area Connection* 5)
-echo 3. (Local Area Connection* 3)
-echo 4. (Local Area Connection* 4)
-echo 5. (Npcap Loopback Adapter)
-
-
-set /p tt=run
-if %tt%==1 goto 1
-if %tt%==2 goto 2
-if %tt%==3 goto 3
-if %tt%==4 goto 4
-if %tt%==5 goto 5
+  echo 1. (VirtualBox Host-Only Network)
+  echo 2. (Local Area Connection* 5)
+  echo 3. (Local Area Connection* 3)
+  echo 4. (Local Area Connection* 4)
+  echo 5. (Npcap Loopback Adapter)
+  set /p tt=run
+  IF %tt%==1 GOTO 1
+  IF %tt%==2 GOTO 2
+  IF %tt%==3 GOTO 3
+  IF %tt%==4 GOTO 4
+  IF %tt%==5 GOTO 5
 
 
 
 
 :1
-  cd c:\"program files"\wireshark\
-
+  cd C:\"program files"\wireshark\
   mkdir "C:\Users\DaVinci\wireshark_logs\%date%-%time%.log"
-
-  tshark  -i 1 > "C:\Users\DaVinci\wireshark_logs\%date%-%time%.log"
-
+  tshark -i 1 > "C:\Users\DaVinci\wireshark_logs\%date%-%time%.log"
   ::-b duration:1800 -w C:\Users\DaVinci\wireshark_logs\
-
-  ::goto create
-  pause
+  ::GOTO create
+  PAUSE
   cls
-  goto menu
-
+  GOTO menu
 
 :2
   start c:\"program files"\wireshark\dumpcap.exe -i 2 -w testfile.txt -c 100
 
-  pause
+  PAUSE
   cls
-  goto menu
+  GOTO menu
 
 :3
   start c:\"program files"\wireshark\dumpcap.exe -i 3 -p
-  pause
+  PAUSE
   cls
-  goto menu
+  GOTO menu
 
 :4
   start c:\"program files"\wireshark\dumpcap.exe -i 4 -p
-  pause
+  PAUSE
   cls
-  goto menu
+  GOTO menu
 
 :5
   start c:\"program files"\wireshark\dumpcap.exe -i 5 -p
-  pause
+  PAUSE
   cls
-  goto menu
-
-
-
+  GOTO menu
 
 :create
 
-
-
-
-
-
-::SET pcap_to_txt=pcap -V -r file_to_convert.pcap > file_to_convert.txt
+  ::SET pcap_to_txt=pcap -V -r file_to_convert.pcap > file_to_convert.txt
   ::echo Converting File %pcap_to_txt% >> "C:\Users\DaVinci\wireshark_logs"
   ::%pcap_to_txt% >> "C:\Users\DaVinci\wireshark_logs" 2>%1
   ::echo Check logs
@@ -136,4 +124,4 @@ REM tshark -T fields -n -r "C:\Users\DaVinci\AppData\Local\Temp\wireshark_Virtua
 
 REM tshark -V -r %1 -T fields -E header=y -e wlan.sa - ip.src -e wlan.da -e ip.dst -E separator=, > %2
 
-pause
+PAUSE
